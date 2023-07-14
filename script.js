eac0c0a850d21d29a3fc0b3cdea49bd4ca0e7123
Surajbarole <surajbarole1@gmail.com>
2023-07-14 20:57:48 +0530

    this is updated code

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+
+gsap.from("#navList",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
+gsap.from("#logo-img",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
+
+
+
 // gsap.to("#menu-btn",{duration: 2, delay: .5,x: "0",ease: "bounce"})
-gsap.from("#menu-btn",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
+gsap.from("#menu-btn",{duration: 2, delay: .5,x: "-250%",ease: "bounce"})
 
 
-gsap.from("#menu-text",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
+gsap.from("#menu-text",{duration: 2, delay: .5,x: "-100%",ease: "bounce"})
 
 // gsap.to(".images-section",{duration: 2, delay: .5,x: "100px",ease: "bounce"})
 // gsap.from(".images-section",{duration: 2, delay: .5,y: "10%",ease: "bounce"})
