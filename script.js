6a44e24f0d62674582239020a1e9273d352eaa09
Surajbarole <surajbarole1@gmail.com>
2023-07-22 21:39:41 +0530

    updated foodsite

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
+var tl=gsap.timeline();
 
-gsap.from("#navList",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
-gsap.from("#logo-img",{duration: 2, delay: .5,y: "-250%",ease: "bounce"})
+tl.from("#nav , #navList",{
+    y:-100,
+    opacity:0,
+    duration:1,
+    stagger:.7
+})
+tl.from(".bannerimg",{
+    scale:1,
+    opacity:0,
+    duration:1,
+    stagger:.3
+})
+tl.from(" #discount, #main-text h1,#main-text p,#main-text ",{
+    x:500,
+    opacity:0,
+    duration:1,
+    stagger:.3
+})
+tl.from("#features",{
+    x:-500,
+    opacity:0,
+    duration:1,
+    stagger:.3
+})
 
 
 
-// gsap.to("#menu-btn",{duration: 2, delay: .5,x: "0",ease: "bounce"})
-gsap.from("#menu-btn",{duration: 2, delay: .5,x: "-250%",ease: "bounce"})
+gsap.from(".grid",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:.3,
+    scrollTrigger : {
+        trigger:".grid",
+        scroller:"body",
+        start:"top 70%",
+        end:"bottom 20%"
+
+    }
+})
+gsap.from("#menu-btn",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:.3,
+    scrollTrigger : {
+        trigger:"#menu-btn",
+        scroller:"body",
+        start:"top 70%",
+
+    }
+})
 
 
-gsap.from("#menu-text",{duration: 2, delay: .5,x: "-100%",ease: "bounce"})
-
-// gsap.to(".images-section",{duration: 2, delay: .5,x: "100px",ease: "bounce"})
-// gsap.from(".images-section",{duration: 2, delay: .5,y: "10%",ease: "bounce"})
 
 
-gsap.from("#footer-section",{duration: 2, delay: .5,y: "300px",ease: "slow"})
+
+gsap.from("#footer-section",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:.3,
+    scrollTrigger : {
+        trigger:"#footer-section",
+        scroller:"body",
+        start:"top 80%"
+    }
+})
